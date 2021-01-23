4c21fbea7dcee81fdcd13225d2068b4a9adbfda7
Alessandro Parma <alessandro_parma@hotmail.it>
2021-01-23 21:04:59 +0100

    add external adaptor template

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,11 +1,21 @@
-def http(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    return f'Hello World!'
+import json
+from adapter import Adapter
+
+
+def gcs_handler(request):
+    adapter = Adapter(request.json)
+    return json.dumps(adapter.result)
+
+
+
+# def http(request):
+#     """Responds to any HTTP request.
+#     Args:
+#         request (flask.Request): HTTP request object.
+#     Returns:
+#         The response text or any set of values that can be turned into a
+#         Response object using
+#         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+#     """
+#     return f'Hello World!'
 
